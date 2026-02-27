34a8ee4ca9943dbf875da46a4c9d1fca813b7716
Vicky <vickynachna@gmail.com>
2026-02-27 19:37:06 +0530

    edit home js

---
+++ js/home.js
@@ -0,0 +1,27 @@
+document.getElementById("playBtn").onclick = function () {
+    let from = document.getElementById("from").value;
+    let to = document.getElementById("to").value;
+    let count = document.getElementById("count").value;
+    let level = document.getElementById("level").value;
+    let sort = document.getElementById("sort").value;
+    if (from === "" || to === "" || count === "") {
+        alert("Please fill all fields!");
+        return;
+    }
+    if (Number(from) >= Number(to)) {
+        alert("Range is invalid!");
+        return;
+    }
+    let gameSettings = {
+        from: Number(from),
+        to: Number(to),
+        count: Number(count),
+        level: level,
+        sort: sort
+    };
+    localStorage.setItem(
+        "gameSettings",
+        JSON.stringify(gameSettings)
+    );
+    window.location.href = "game.html";
+};
